bef91c31557caddf17806f14b9bbc13772606856
Muaadahmed <muaad.abdi.ahmed@gmail.com>
2022-01-07 05:33:50 +0000

    Create an about page route and an about controller, include a link, and render an erb template for the page

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,5 @@
+class AboutController < ApplicationController
+  def index
+    @about = "Jungle is a mini e-commerce application built with Rails 4.2"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -17,6 +18,7 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  get '/about' => 'about#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
