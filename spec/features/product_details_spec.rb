519184463f151544a72efe42948e56554ab07d9f
Muaadahmed <muaad.abdi.ahmed@gmail.com>
2022-01-14 02:30:45 +0000

    Implement test for when a use clicks on details button on the product

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    1.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "See product details when visitor clicks on the product" do
+    visit root_path
+
+    click_on 'Details'
+  end
+end
